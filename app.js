c294326e8d3c6d26d75012b22bf9b68a706cd303
Francis Chima <79450187+youngkvng@users.noreply.github.com>
2021-04-22 10:27:04 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,182 @@
+// // // alert('Hello World');
+// // /*
+// // console.log('Hello World');
+// // console.log(123);
+// // console.log(true);
+
+// // let greeting = 'hello';
+// // console.log(greeting);
+// // console.log([1,2,3,4,5,6,7,8,]);
+// // console.log({a:1, b:2, c:3, d:'four'});
+// // console.table({a:1, b:2, c:3, d:'four'});
+// // console.error('this is some error');
+// // console.clear();
+// // console.warn('this is a warning')
+
+// // console.time('hello')
+// // console.log(123);
+// // console.log(123);
+// // console.log(123);
+// // console.log(123);
+// // console.log(123);
+// // console.log(123);
+// // console.log(123);
+// // console.log(123);
+// // console.timeEnd('hello');
+// // console.clear();
+// // */
+
+// // // var name = 'john doe';
+// // // console.log(name);
+
+// // // name = 'steve doe';
+// // // console.log(name)
+
+
+// // //SIMPLE MATHS WITH NUMBER
+// // // const num1 = 100;
+// // // const num2 = 50;
+// // // let val;
+// // // val = num1 + num2;
+// // // val = num1 * num2;
+// // // val = num1 - num2;
+// // // val = num1 / num2;
+// // // val = num1 % num2;
+
+// // // //MATHS OBJECT
+// // // val = Math.exp(2, 5)
+// // // val = Math.round(3.6)
+// // // val = Math.PI
+// // // val = Math.E
+// // // val = Math.pow(3,3)
+// // // console.log(val);
+
+
+// // // const today = new Date();
+// // // let val;
+
+// // // val = today.getMonth();
+// // // val = today.getDate();
+// // // val = today.getDay();
+// // // // val = today.getTime();
+// // // val = today.getFullYear()
+// // // console.log(val);
+
+
+// // const id = 100;
+
+// // //EQUAL TO ==
+
+// // // if (id == 180){
+// // //     console.log('correct')
+// // // }else{
+// // //     console.log('incorrect')
+// // // }
+
+// // // NOT EQUAL TO !=
+
+// // if (id != 100){
+// //     console.log('correct')
+// // }else{
+// //     console.log('incorrect')
+// // }
+
+
+// let day;
+// switch (new Date().getDay()) {
+//     case 0:
+//         day = 'sunday'
+//         break;
+//     case 1:
+//         day = 'monday'
+//         break;
+//     case 2:
+//         day = 'tuesday'
+//         break;
+//     case 3:
+//         day = 'wednesday'
+//         break;
+//     case 4:
+//         day = 'thursday'
+//         break;
+//     case 5:
+//         day = 'friday'
+//         break;
+//     case 6:
+//         day = 'saturday'
+//         break;
+
+//     default:
+//         break;
+// }
+// console.log(day)
+
+
+
+
+
+//WINDOWS METHODS / OBJECTS / PROPERTIES
+
+//ALERT
+// alert('Hello World');
+
+// //PROMPT
+// const input = prompt();
+// alert(input);
+
+//COMFIRM
+//  if(confirm('comfirm delete process')){
+//      console.log('yes')
+//  }
+
+// let val;
+// //OUTER HEIGHT AND WIDTH
+// val = window.outerHeight;
+// val = window.outerWidth;
+
+// //INNER HEIGHT AND WIDTH
+// val = window.innerHeight;
+// val = window.innerWidth;
+
+// //SCROLL POINT
+// val = window.scrollY;
+// val = window.scrollX;
+
+// //LOCATION OBJECT
+// val = window.location;
+// val= window.location.hostname;
+// val= window.location.port;
+// val= window.location.href;
+// val= window.location.search;
+
+
+// window.location.href = 'http://google.com';
+// window.location.reload();
+
+//HISTORY OBJECT
+
+// window.history.go(-1)
+
+// //NAVIGATOR OBJECT
+
+// val = window.navigator;
+// val = window.navigator.appName;
+// val = window.navigator.appVersion;
+
+// console.log(val);
+
+let val;
+val = document;
+val = document.all;
+val = document.all[1];
+val = document.all.length;
+val = document.head;
+val = document.body;
+val = document.doctype;
+val = document.domain;
+val = document.URL;
+val = document.characterSet;
+
+
+
+console.log(val)
